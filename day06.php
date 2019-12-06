2bcdb17bd7af2cc308b66159decc31269691ef54
mariush-github <mariush@helpedia.com>
2019-12-06 13:18:14 +0200

    Day 6 (simplified)

--- day06.php
+++ day06.php
@@ -1,41 +1,43 @@
 <?php
 
+// see https://adventofcode.com/2019/day/6
+
+
 $items_orbits = array();
 $items_unique = array();
 
 $filename = __DIR__ . '/inputs/06.txt';
+
 $lines = explode(chr(0x0A),str_replace(chr(0x0D).chr(0x0A),chr(0x0A),file_get_contents($filename)));
 if (count($lines)<1) die('No entries read. Maybe filename not found?');
 foreach ($lines as $line) {
     if (trim($line)!='') {
         $parts = explode(')',$line);
+        // making a list of unique names to make it easier later and show count on screen
         if (isset($items_unique[$parts[0]])==FALSE) $items_unique[$parts[0]] = TRUE;
         if (isset($items_unique[$parts[1]])==FALSE) $items_unique[$parts[1]] = TRUE;
+        // store the relation (x orbits y) in array
         $items_orbits[$parts[1]] = $parts[0];
     }
 }
 
 function counter($name) {
     global $items_orbits;
+ 
     $list = chain($name);
 
-    $show_indirects = FALSE; // set to true to see the indirects.
-    $directs = 0;
-    $indirects = 0;
     $nr = count($list);
     if ($nr==0) echo "$name does not orbit anything.";
-    if ($nr>0) echo "$name directly orbits ".$list[$nr-1];
-    if ($nr>1)  {
-        echo ' and indirectly orbits '.($nr-1).' planets.';
-    }
+    if ($nr >0) echo "$name directly orbits ".$list[0];
+    if ($nr >1) echo ' and indirectly orbits '.($nr-1).' planets.';
     echo "\n";
-
     return $nr;
 }
 
 function chain($name) {
     global $items_orbits;
     $list = array();
+    if (isset($items_orbits[$name])==FALSE) return $list;
     $temp = $items_orbits[$name];
     while ($temp!='') {
         array_push($list,$temp);
