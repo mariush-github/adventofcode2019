c9e28b910ff0c6a3b45114f272340ce703f29a6d
mariush-github <mariush@helpedia.com>
2019-12-06 13:06:32 +0200

    Day 6

---
+++ day06.php
@@ -0,0 +1,71 @@
+<?php
+
+$items_orbits = array();
+$items_unique = array();
+
+$filename = __DIR__ . '/inputs/06.txt';
+$lines = explode(chr(0x0A),str_replace(chr(0x0D).chr(0x0A),chr(0x0A),file_get_contents($filename)));
+if (count($lines)<1) die('No entries read. Maybe filename not found?');
+foreach ($lines as $line) {
+    if (trim($line)!='') {
+        $parts = explode(')',$line);
+        if (isset($items_unique[$parts[0]])==FALSE) $items_unique[$parts[0]] = TRUE;
+        if (isset($items_unique[$parts[1]])==FALSE) $items_unique[$parts[1]] = TRUE;
+        $items_orbits[$parts[1]] = $parts[0];
+    }
+}
+
+function counter($name) {
+    global $items_orbits;
+    $list = chain($name);
+
+    $show_indirects = FALSE; // set to true to see the indirects.
+    $directs = 0;
+    $indirects = 0;
+    $nr = count($list);
+    if ($nr==0) echo "$name does not orbit anything.";
+    if ($nr>0) echo "$name directly orbits ".$list[$nr-1];
+    if ($nr>1)  {
+        echo ' and indirectly orbits '.($nr-1).' planets.';
+    }
+    echo "\n";
+
+    return $nr;
+}
+
+function chain($name) {
+    global $items_orbits;
+    $list = array();
+    $temp = $items_orbits[$name];
+    while ($temp!='') {
+        array_push($list,$temp);
+        $temp = isset($items_orbits[$temp])==TRUE ? $items_orbits[$temp] : '';
+    }
+    return $list;
+}
+
+$direct = 0;
+$indirect = 0;
+$total = 0;
+
+echo "There are ".count($items_unique)." planets and ".count($items_orbits)." orbits.\n";
+
+foreach ($items_unique as $name => $value) {
+    $result = counter($name);
+    $total += $result;
+}
+echo 'Total orbits: '.$total."\n";
+
+$o1 = chain('YOU');
+$o2 = chain('SAN');
+echo "Orbit chain of YOU:".json_encode($o1)."\n";
+echo "Orbit chain of SAN:".json_encode($o2)."\n";
+$offset1 = count($o1)-1;
+$offset2 = count($o2)-1;
+while ($o1[$offset1]==$o2[$offset2]) { 
+    $offset1--;
+    $offset2--;
+}
+echo "Orbital jumps required: ".($offset1+$offset2+2)." ($offset1 + $offset2 + 2)\n";
+
+?>
